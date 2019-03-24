d2ea70514546d6eee7bfab7ac6a8b8d3920df69d
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-03-24 14:18:11 +0530

    Update 109.c

--- 109.c
+++ 109.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
-#include<conio.h>
-int main()
+#include<stdio.h>
+void main()
 {
-	int a[100],b,c=0,d[100],i,j,k=0;
-	scanf("%d",&b);
-	for(i=0;i<b;i++)
-	scanf("%d",&a[i]);
-	for(i=0;i<b;i++)
-	{
-	c=c+a[i];
-	d[i]=c;
-	//	printf("%d\t",d[i]);
-	}
-	for(i=b-1;i>=0;i--)
-	printf("%d\t",d[i]);
+    int a[100],i,j,n;
+    clrscr();
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-2;i>=0;i--)
+    {
+        a[i]=a[i+1]+a[i];
+    }
+     for(i=0;i<n;i++) 
+     {
+         printf("%d ",a[i]);
+     }
+    getch();
 }
